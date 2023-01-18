25a967831b440da0d9b871e34de6fa82e98233d4
SRINIVASAN N R <srinivasannr99@gmail.com>
2023-01-18 21:00:10 +0530

    pizza1

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,17 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        if(this.flagChe==0 && this.flagTop==0){
+            this.flagChe=1;
+            this.flagTop=1;
+            setPrice(getPrice()+80);
+            if(isVeg==true){
+                setPrice(getPrice()+70);
+            }
+            else{
+                setPrice(getPrice()+120);
+            }
+        }
         // your code goes here
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,12 +3,31 @@ package com.driver;
 public class Pizza {
 
     private int price;
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     private Boolean isVeg;
     private String bill;
 
+    int flagChe=0;
+    int flagTop=0;
+
+    int flagTake=0;
+
     public Pizza(Boolean isVeg){
+        this.bill="";
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg==true){
+            this.bill+="Base Price Of The Pizza: 300"+"\n";
+            this.price+=300;
+        }
+        else{
+            this.bill+="Base Price Of The Pizza: 400"+"\n";
+            this.price+=400;
+        }
     }
 
     public int getPrice(){
@@ -17,18 +36,39 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(this.flagTop==0 && this.flagChe==0) {
+            this.bill+="Extra Cheese Added: 80"+"\n";
+            this.price += 80;
+            this.flagChe=1;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(this.flagTop==0 && this.flagChe==1) {
+            if (isVeg == true) {
+                this.bill+="Extra Toppings Added: 70"+"\n";
+                this.price += 70;
+            } else {
+                this.bill+="Extra Toppings Added: 120"+"\n";
+                this.price += 120;
+            }
+            this.flagTop = 1;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(flagTake==0) {
+            this.bill += "Paperbag Added: 20" + "\n";
+            this.price += 20;
+            this.flagTake=1;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        this.bill+="Total Price: "+this.price;
         return this.bill;
     }
 }
